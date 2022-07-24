eb248a7ddfc026011323b61022e815e7f4da7b64
Liyahagos <103388423+Liyahagos@users.noreply.github.com>
2022-07-24 12:48:36 -0700

    Create dom-play.js

---
+++ dom-play.js
@@ -0,0 +1,41 @@
+//alert ("HI");
+
+function myAlert(){
+
+    alert("clicked");
+    
+    }
+
+const span  = document.querySelectorAll("#play span" );
+
+console.log(spans);
+
+for (const mySpan of spans) {
+//mySpan.addEventListener("click",myAlert);
+//alert(mySpan.dataset.actor);
+mySpan.addEventListener("click",function(ev){
+   // alert("THe current actor is" + mySpan.dataset.actor);
+highlighActor(mySpan.datase.actor);
+});
+}
+
+
+
+
+
+
+
+
+
+function highlighActor (actor) {
+
+    for(const mySpan of spans) {
+
+        if(actor== mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor=='yellow';
+        }else{//other actor, bakcground white
+            mySpan.style.backgroundColor=='white';
+        }
+    }
+    
+}
